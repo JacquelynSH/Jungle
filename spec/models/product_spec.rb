f1916cdb25185f130d0220abdfe9bbd42e832834
JacquelynSH <elpatchaembroidery@gmail.com>
2022-11-09 13:55:24 -0800

    Add error message check

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,24 +14,28 @@ RSpec.describe Product, type: :model do
       @category = Category.new(name: "Trees")
       @product = Product.new(name: nil, price: "24", quantity: "5", category: @category)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end 
 
     it 'should not create product if price is empty' do
       @category = Category.new(name: "Trees")
       @product = Product.new(name: "Plant", price_cents: nil, quantity: "5", category: @category)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end 
 
     it 'should not create product if quantity is empty' do
       @category = Category.new
       @product = Product.new(name: "Plant", price: "24", quantity: nil, category: @category)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end 
 
     it 'should not create product if category is empty' do
       @category = Category.new
       @product = Product.new(name: "Plant", price: "24", quantity: "24", category: nil)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end 
   end
 end
