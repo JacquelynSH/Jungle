b58597084dafd02efbb168f6a80df7831cf9deb2
JacquelynSH <elpatchaembroidery@gmail.com>
2022-11-10 17:26:57 -0800

    Product tests added

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,10 +3,14 @@
 describe('goes to Jungle homepage', () => {
   
   it("visits the homepage", () => {
-    cy.visit("http://localhost:3000/")
+    cy.visit("/")
   }) 
   
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
   });
 })
